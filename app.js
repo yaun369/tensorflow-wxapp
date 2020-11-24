e090059ab7561c0ac2a6f3ab8ea6c7b399a2e1db
yuanmengmeng <geekreading@163.com>
2020-11-24 22:27:47 +0800

    feat: init project.

---
+++ app.js
@@ -0,0 +1,20 @@
+//app.js
+var fetchWechat = require('fetch-wechat');
+var tf = require('@tensorflow/tfjs-core');
+var webgl = require('@tensorflow/tfjs-backend-webgl');
+var plugin = requirePlugin('tfjsPlugin');
+App({
+  onLaunch: function () {
+    plugin.configPlugin({
+      // polyfill fetch function
+      fetchFunc: fetchWechat.fetchFunc(),
+      // inject tfjs runtime
+      tf,
+      // inject webgl backend
+      webgl,
+      // provide webgl canvas
+      canvas: wx.createOffscreenCanvas()
+    });
+    // tf.tensor([1, 2, 3, 4]).print();
+  }
+})
